e39d02ba4a0aceecb435eb00da690d8ab25c8d6a
amirafatnassi <amirafatnassi88@gmail.com>
2022-10-01 11:47:36 +0100

    displaying error & qelect control validation

--- tdf/src/app/app.component.ts
+++ tdf/src/app/app.component.ts
@@ -9,5 +9,15 @@ import { User } from './user';
 export class AppComponent {
   title = 'tdf';
   topics = ['angular', 'react', 'vue'];
-  userModel = new User('rob', 'rob@test.com', 343232, '', 'morning', true);
+  topicHasError = true;
+
+  userModel = new User('rob', 'rob@test.com', 343232, 'default', 'morning', true);
+
+  validateTopic(value: string) {
+    if (value === 'default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
+  }
 }
